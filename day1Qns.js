00c4a002eb9ccfb154c9ed5f1734f48c0cca83aa
Amar Gupta <theamargupta.tech@gmail.com>
2023-06-05 01:16:03 +0530

    day 1 qns

---
+++ day1Qns.js
@@ -0,0 +1,59 @@
+// What is the purpose of the DOCTYPE declaration in HTML?
+
+// What is the role of the <head> element in an HTML document?
+
+// Explain the purpose of the <body> element.
+
+// What are the different heading tags in HTML and how are they used?
+
+// How do you create a hyperlink in HTML?
+
+// How do you insert an image in HTML?
+
+// What are the differences between unordered and ordered lists in HTML?
+
+// How do you create a division or container element in HTML?
+
+// What is the purpose of the <span> tag in HTML?
+
+// Explain the concept of block-level and inline elements in HTML.
+
+// How do you create a new paragraph in HTML?
+
+// What is the use of the <br> tag in HTML?
+
+// How do you add comments in an HTML document?
+
+// How do you create a table in HTML?
+
+// What is the role of the <form> tag in HTML?
+
+// How do you create a drop-down menu in HTML?
+
+// What is the purpose of the <input> tag in HTML forms?
+
+// How do you include special characters in HTML, such as < and >?
+
+// How do you create an ordered list with Roman numerals?
+
+// Explain the concept of semantic HTML and provide examples of semantic tags.
+
+// How do you add a background image to an HTML element?
+
+// What is the purpose of the <iframe> tag in HTML?
+
+// How do you create a responsive web design using HTML and CSS?
+
+// Explain the concept of HTML5 and its new features.
+
+// How do you include external CSS stylesheets in an HTML document?
+
+// What is the role of the <meta> tag in HTML?
+
+// How do you create a form with input fields and submit button in HTML?
+
+// What are the different types of HTML input fields?
+
+// How do you embed videos or audio files in an HTML document?
+
+// Explain the concept of accessibility in HTML and the use of ARIA attributes.
