49577c3ec2dc5a7b331f9ace2cc2d45947787b5f
Amar Gupta <theamargupta.tech@gmail.com>
2023-06-05 01:19:02 +0530

    day2

---
+++ day2Qns.js
@@ -0,0 +1,30 @@
+// How do you select an element with the ID "header" in CSS?
+// What is the difference between the classes and ID selectors in CSS?
+// How do you select all <p> elements with a class of "intro"?
+// How do you select the first child element of a parent element in CSS?
+// What is the CSS property used to set the font color of an element?
+// How do you select all elements within a specific HTML class?
+// What is the CSS property used to set the background color of an element?
+// How do you select only the last child element of a parent element in CSS?
+// What is the CSS property used to set the font size of an element?
+// How do you select all <a> elements that have a "target" attribute set to "_blank"?
+// What is the CSS property used to set the width of an element?
+// How do you select all elements that are descendants of a specific HTML element?
+// What is the CSS property used to set the height of an element?
+// How do you select all elements with a specific attribute value in CSS?
+// What is the CSS property used to set the margin of an element?
+// How do you select the first element with a specific class in CSS?
+// What is the CSS property used to set the padding of an element?
+// How do you select all elements that are direct children of a specific HTML element?
+// What is the CSS property used to set the text alignment of an element?
+// How do you select all elements that have a specific attribute in CSS?
+// What is the CSS property used to set the font family of an element?
+// How do you select elements based on their position in the HTML structure?
+// What is the CSS property used to set the text decoration of an element?
+// How do you select elements based on their state, such as hover or active?
+// What is the CSS property used to set the display behavior of an element?
+// How do you select all elements with a specific class and a specific attribute in CSS?
+// What is the CSS property used to set the border of an element?
+// How do you select elements based on their attribute values using attribute selectors?
+// What is the CSS property used to set the opacity of an element?
+// How do you select all elements that are siblings of a specific HTML element?
