326261a03007baa90a6ce82eca8d09c742f1929d
Anisha Gupta <anishagupta@Anishas-MacBook-Air.local>
2023-06-05 22:43:26 +0530

    Day 1 - Answers inside answer.js file

---
+++ answer.js
@@ -0,0 +1,171 @@
+//Answer 1
+//<!DOCTYPE html>
+/* 
+Doctype declaration is an information to the browser about what document type to expect.
+And without this tag the html code will not be accepted by the browser.
+*/
+
+//////////////////////////////////////
+
+// Answer 2
+/*
+The <head> element is a container for metadata (data about data) and is placed between the <html> tag and the <body> tag.
+
+Metadata is data about the HTML document. Metadata is not displayed.
+
+Metadata typically define the document title, character set, styles, scripts, and other meta information.
+
+The following elements can go inside the <head> element:
+
+<title> (required in every HTML document)
+<style>
+<base>
+<link>
+<meta>
+<script>
+<noscript>
+*/
+
+//Answer 3
+/*
+It is the body of the html. Body (</body>) element contains all the contents of an HTML document, such as headings, paragraphs, images, hyperlinks, tables, lists, etc.
+Note: There can only be one <body> element in an HTML document.
+*/
+
+//answer 4
+/*
+There are 6 tags - h1 to h6.
+h1 is for the most impportant heading and h6 is for the least important heading.
+*/
+
+//Answer 5
+/*
+<a href="" target="_blank"/>
+*/
+
+//Answer 6
+/**
+ * <img src="" alt="" />
+ */
+
+//Answer 7
+/**
+ * <ul>This list shows the data in unordered manner</ul>
+ * <ol>This list shows the data in an order.</ol>
+ */
+
+//Answer 8
+/**
+ * <div></div>
+ */
+
+//Answer 9
+/**
+ * <span>This tag is used for highlighting the text. This is an inline element.</span>
+ */
+
+//Answer 10
+/**
+ * 
+A block-level element always starts on a new line and takes up the full width available. 
+An inline element does not start on a new line and it only takes up as much width as necessary.
+ */
+
+//Answer 11
+/**
+ * <p></p>
+ */
+
+//Answer 12
+/**
+ * <br/> tag is used to break the text in between the line of the content.
+ */
+
+//Answer 13
+/**
+ * <!-- -->
+ *
+ */
+
+//Answer 14
+/**
+ * <table></table>
+ */
+
+//Answer 15
+/** <form> This is used to create a form which is used for gathering the data from the user.</form>*/
+
+//Answer 16
+/**
+ * <dropdown>
+ * <select option="1"></select>
+ * <select option="2"></select>
+ * <select option="3"></select>
+ * <select option="4"></select>
+ * </dropdown>
+ */
+
+//Answer 17
+/** <input/> to collect input from the user*/
+
+//Answer 18
+/** 
+ * <ol type="i">
+   <li>Item in list…</li>
+   <li>Item in list…</li>
+   <li>Item in list…</li>
+</ol>
+*/
+//Answer 19
+/** A semantic element clearly describes its meaning to both the browser and the developer.
+ * Examples of non-semantic elements: <div> and <span> - Tells nothing about its content. Examples of semantic elements: <form> , <table> , and <article> - Clearly defines its content.
+ *
+ * */
+//Answer 20
+/**
+ * baqckground-img : utl("")
+ *
+ */
+//Answer 21
+/** 
+ * 
+an inline frame
+The <iframe> tag specifies an inline frame. 
+An inline frame is used to embed another document within the current HTML document.
+*/
+//Answer 22
+/** Using media query, we can create responsive website.*/
+//Answer 23
+/** HTML5 is a better and advanced version of HTML. It has <em></em>,<article></article>,<heaer></heaer>,<footer></footer>,<section></section>, etc tags wich are semantic tags.  */
+//Answer 24
+/** For using the css externally, create a style file and attach this file with the help of <link /> tag inside the <head></head> tag in the html.*/
+//Answer 25
+/** <meta /> tag is used to describe the meta data (data about data). Such as links, scripts, title, charset, etc.*/
+//Answer 26
+/**
+ * A form is used for collecting user data and submit is used to run a query to the backend in order to save the data in the database.
+ *
+ */
+
+//Answer 27
+/**
+ * Input types are -- Number,tel,button,image,text,file,email,etc.
+ *
+ */
+//Answer 27
+/**
+ * <audio>
+    <source src="file_name" type="audio_file_type">
+</audio>
+
+<video>
+    <source src="file_name" type="video_file_type">
+</video>
+ *
+ */
+//Answer 27
+/**
+ *Through the use of ARIA, authors can specify semantics that go beyond the current capabilities of native HTML. 
+ This can be very useful, as it provides authors the opportunity to create widgets, or expose specific accessible states and properties to native HTML features which would not be possible by the use of HTML alone.
+ *
+ */
