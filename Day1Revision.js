c750670b3ea83beb866bf94ab2aaeb45405bb129
Anisha Gupta <anishagupta@Anishas-MacBook-Air.local>
2023-06-06 22:08:52 +0530

    Day 2 - Answers inside answer.js file

--- Day1Revision.js
+++ Day1Revision.js
@@ -1,10 +1,10 @@
 // What is the difference between HTML and XHTML?
 // Explain the box model in HTML and how it affects layout.
-// How does the "doctype" declaration in HTML impact the rendering of a web page?
-// What are the differences between "display: block", "display: inline", and "display: inline-block" in CSS?
-// Describe the purpose and usage of HTML5 semantic elements, such as <header>, <nav>, and <section>.
-// How can you make an HTML element responsive and adjust its layout based on different screen sizes?
+// How does the "doctype" declaration in HTML impact the rendering of a web page? // to make the html code understandable by the browser that wat type of text it is.
+// What are the differences between "display: block", "display: inline", and "display: inline-block" in CSS? //
+// Describe the purpose and usage of HTML5 semantic elements, such as <header>, <nav>, and <section>. // semantic html, to understand by the tag name only
+// How can you make an HTML element responsive and adjust its layout based on different screen sizes? // media query
 // Explain the concept of accessibility in HTML and provide examples of HTML attributes that can improve accessibility.
 // Describe the differences between the <script>, <script async>, and <script defer> attributes when including JavaScript in HTML.
-// What are data attributes in HTML and how can they be used?
+// What are data attributes in HTML and how can they be used? 
 // Explain the concept of HTML entities and give examples of commonly used entities.
---
+++ answer.js
@@ -0,0 +1,57 @@
+/**
+ * ANSWER 1 - #header
+ * ANSWER 2 - .class & #id. 
+ * Id is used for a specific tag/element but classes can be used multiple elements.
+ * ANSWER 3 - .intro p {}
+ * ANSWER 4 - .parent-element :first-child {}
+ * ANSWER 5 - color:white
+ * ANSWER 6 - .class {}
+ * ANSWER 7 - background-color : green
+ * ANSWER 8 - .parent-element :last-child {}
+ * ANSWER 9 - font-size : 20px
+ * ANSWER 10 - a[target="_blank"] {}
+ * ANSWER 11 - width : 50px
+ * ANSWER 12 - element p {}
+ * ANSWER 13 - height : 50px
+ * ANSWER 14 - a[href="https://example.com"] {}, input[type="text"] {}, a[target="_blank"] {}
+ * ANSWER 15 - margin : 10px
+ * ANSWER 16 - .my-class:first-of-type {color: red}
+ * ANSWER 17 - PADDING : 4PX
+ * ANSWER 18 - div.parent-class > p {}
+ * ANSWER 19 - text-align : right
+ * ANSWER 20 - a[href="https://example.com"] {}
+ * ANSWER 21 - font-family - arial;
+ * ANSWER 22 - .p:first-of-type {}, li:last-child {}, etc.
+ * ANSWER 23 - p { text-decoration: underline }
+ * ANSWER 24 - a:hover{}
+ * ANSWER 25 - display : none 
+ * ANSWER 26 - .elementClass[attributeName="attributeValue"]{}
+ * ANSWER 27 - border : 2px solid blue
+ * ANSWER 28 - [attribute$=".jpg"] {}
+ * ANSWER 29 - opacity : 1
+ * ANSWER 30 - div.parent-class ~ span {}
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
