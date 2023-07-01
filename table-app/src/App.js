d591d5fcb6f84d1b30c85f4f1cc1ed5b736798c8
Amar Gupta <theamargupta.tech@gmail.com>
2023-07-02 00:38:07 +0530

    fix/ag/dashbaord change

--- table-app/src/App.js
+++ table-app/src/App.js
@@ -7,50 +7,43 @@ const App = () => {
   const [data, setData] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [searchedData, setSearchedData] = useState([]);
-  const [companyValue, setCompanyValue] = useState([]);
-  const [countryValue, setCountryValue] = useState([]);
-  const [contactValue, setContactValue] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
+    const loadUsersData = async () => {
+      return await axios
+        .get("http://localhost:3006/users")
+        .then((res) => {
+          setData(res?.data);
+          setSearchedData(res?.data);
+        })
+        .catch((err) => console.log("err", err));
+    };
     loadUsersData();
   }, []);
 
-  const loadUsersData = async () => {
-    return await axios
-      .get("http://localhost:3006/users")
-      .then((res) => {
-        setData(res?.data);
-        const selectCompanyValues = getValueByKey(res?.data, "Company");
-        const selectCountryValues = getValueByKey(res?.data, "Country");
-        const selectContactValues = getValueByKey(res?.data, "Contact");
-        setCompanyValue(selectCompanyValues);
-        setCountryValue(selectCountryValues);
-        setContactValue(selectContactValues);
-      })
-      .catch((err) => console.log("err", err));
-  };
-
   const getValueByKey = (arr, key) => arr.map((obj) => obj[key]);
 
-  const handleReset = () => {
-    setSearchInput("");
-    setSearchedData([]);
-  };
-
   const handleSearch = async (e) => {
-    if (e?.preventDefault) {
-      e.preventDefault();
-    }
-    if (searchInput !== " ") {
-      const searchedInput = data.filter((data) =>
-        Object.values(data)
-          .join(" ")
-          .toLowerCase()
-          .includes(searchInput.toLowerCase())
-      );
-      setSearchedData(searchedInput);
-    }
+    e.preventDefault();
+    const searchedInput = data.filter((data) =>
+      Object.values(data)
+        .join(" ")
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase())
+    );
+    setSearchInput(e.target.value);
+    setSearchedData(searchedInput);
+    // }
+    // Line 46 (data.filter((data) =>)=> iteration each array ke saare indexes[{country:"Wipro",Contact:"Anisha"...}....] => {country:"Wipro",Contact:"Anisha"...}
+
+    // Line 47 (Object.values(data)) =>{country:"Wipro",Contact:"Anisha"...}=> ["Anisha","Wipro", ...]
+
+    // Line 48 => ["Anisha","Wipro", ...] =>"Ansiha Wipro ....."
+
+    // Line 50 => "Ansiha Wipro ....." ("anisha")=>  True or False
+
+    // Important => => =>
     // return await axios
     //   .get(`http://localhost:3006/users?q=${searchInput}`)
     //   .then((res) => {
@@ -64,39 +57,30 @@ const App = () => {
   return (
     <div className="main-container">
       <h1 className="first-heading">Table</h1>
-      <form onSubmit={handleSearch} className="form">
+      <div className="form">
         <input
           className="search"
           name="search"
           placeholder="Search"
           value={searchInput}
-          onChange={(e) => setSearchInput(e.target?.value)}
+          onChange={handleSearch}
         />
-        <button className="search-btn" onClick={() => handleSearch()}>
-          Search
-        </button>
-        <button className="reset" onClick={() => handleReset()}>
-          Reset
-        </button>
-        <button className="filter" onClick={() => setModalOpen(true)}>
+        <button className="filter" onClick={() => setModalOpen((value)=>!value)}>
           Filter
         </button>
-      </form>
+      </div>
       {modalOpen && (
         <Modal
-          companyValue={companyValue}
-          countryValue={countryValue}
-          contactValue={contactValue}
+          companyValue={getValueByKey(searchedData, "Company")}
+          countryValue={getValueByKey(searchedData, "Country")}
+          contactValue={getValueByKey(searchedData, "Contact")}
           modalOpen={modalOpen}
           setModalOpen={setModalOpen}
         />
       )}
       <br />
-      {searchedData.length > 0 ? (
-        <Table usersData={searchedData} />
-      ) : (
-        <Table usersData={data} />
-      )}
+
+      <Table usersData={searchedData} />
     </div>
   );
 };
--- table-app/src/Components/Table.js
+++ table-app/src/Components/Table.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "./Table.css";
 export const Table = ({ usersData }) => {
-    
   return (
     <>
-        <table id="customers">
-          <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-            {/* <th>Action</th> */}
-          </tr>
-          {usersData?.map((data,id) => {
-            return (
-              <tr key={id}>
-                <td>{data.Company}</td>
-                <td>{data.Contact}</td>
-                <td>{data.Country}</td>
-                {/* <th>Action</th> */}
-              </tr>
-            );
-          })}
-        </table>
-   
+      <table id="customers">
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+          {/* <th>Action</th> */}
+        </tr>
+        {usersData?.length
+          ? usersData.map((data, id) => {
+              return (
+                <tr key={id}>
+                  <td>{data.Company}</td>
+                  <td>{data.Contact}</td>
+                  <td>{data.Country}</td>
+                  {/* <th>Action</th> */}
+                </tr>
+              );
+            })
+          : "No data Available"}
+      </table>
     </>
   );
 };
